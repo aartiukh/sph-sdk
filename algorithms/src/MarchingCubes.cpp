7c3a76a44df1efe07c4bf46343f65b16a1c29575
crosby98 <ihor.lytvyn@nure.ua>
2019-05-26 13:35:10 +0300

    added MarchingCubes

---
+++ algorithms/src/MarchingCubes.cpp
@@ -0,0 +1,14 @@
+/**
+* @file MarchingCubes.cpp
+* @author Ihor Lytvyn(litvinigor98@gmail.com)
+* @date Created May 26, 2019
+**/
+
+namespace SPHAlgorithms
+{
+
+} //SPHAlgorithms
+
+int main() {
+	return 0;
+}
---
+++ algorithms/src/MarchingCubes.h
@@ -0,0 +1,16 @@
+/**
+* @file MarchingCubes.h
+* @author Ihor Lytvyn(litvinigor98@gmail.com)
+* @date Created May 26, 2019
+**/
+
+#ifndef MARCHING_CUBES_H_43C34465A6ED4DB9B9F2F4C3937BF5DC
+#define MARCHING_CUBES_H_43C34465A6ED4DB9B9F2F4C3937BF5DC
+
+
+namespace SPHAlgorithms
+{
+
+} //SPHAlgorithms
+
+#endif // MARCHING_CUBES_H_43C34465A6ED4DB9B9F2F4C3937BF5DC
