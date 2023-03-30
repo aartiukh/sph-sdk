a6d3562c67bb85283cf778ec857246e25bc277ac
Polina Artiukh <polina.zemko@rallyware.com>
2023-03-30 13:17:47 +0200

    First dirty solution

--- algorithms/benchmark/find_cube.py
+++ algorithms/benchmark/find_cube.py
@@ -6,16 +6,16 @@ FIELD_SIZE = 1.0
 
 
 def find_cubes(points: np.ndarray, search_radius: float) -> np.ndarray:
-    cols = xy[:, 0] // SEARCH_RADIUS
-    rows = xy[:, 1] // SEARCH_RADIUS
+    cols = points[:, 0] // SEARCH_RADIUS
+    rows = points[:, 1] // SEARCH_RADIUS
     cube_size = FIELD_SIZE / SEARCH_RADIUS
     cube_ids = rows * cube_size + cols
     cube_ids = cube_ids.astype(int)
     return cube_ids
 
 
-def _find_neighbor_cubes(cube_id: int, cube_size: float) -> list:
-    cube_neighbors_ids = [
+def _find_neighbor_cubes(cube_id: int, cube_size: int) -> np.array:
+    cube_neighbors_ids = np.array([
         cube_id - 1,
         cube_id + 1,
         cube_id - cube_size,
@@ -24,20 +24,19 @@ def _find_neighbor_cubes(cube_id: int, cube_size: float) -> list:
         cube_id + cube_size + 1,
         cube_id - cube_size - 1,
         cube_id - cube_size + 1
-    ]
+    ], dtype=int)
     return cube_neighbors_ids
 
 
 def find_neighbor_cubes(cube_ids: np.ndarray) -> np.ndarray:
-    cube_size = FIELD_SIZE / SEARCH_RADIUS
-    cubes_neighbors_ids = np.array([])
+    cube_size = int(FIELD_SIZE / SEARCH_RADIUS)
+    cubes_neighbors_ids = []
     for cube_id in cube_ids:
         neighbors = _find_neighbor_cubes(cube_id, cube_size)
-        print(neighbors)
-        cubes_neighbors_ids = np.append(cubes_neighbors_ids, neighbors)
+        cubes_neighbors_ids.append(neighbors)
     # cubes_neighbors_ids = [
     #     _find_neighbor_cubes(cube_id, cube_size) for cube_id in cube_ids]
-    # cubes_neighbors_ids = np.ndarray(cubes_neighbors_ids)
+    cubes_neighbors_ids = np.array(cubes_neighbors_ids)
     return cubes_neighbors_ids
 
 
@@ -49,7 +48,7 @@ if __name__ == '__main__':
     ])
 
     cubes = find_cubes(xy, search_radius=SEARCH_RADIUS)
-    print(cubes)
+    # print(cubes)
 
     cube_nb = find_neighbor_cubes(cubes)
     print(cube_nb)
