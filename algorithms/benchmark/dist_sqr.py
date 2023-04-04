e1e7c81706702bbd65fb5b3b76f94696b0a74311
Polina Artiukh <polina.zemko@rallyware.com>
2023-04-05 01:07:35 +0200

    replace range-len with enumerate

--- algorithms/benchmark/dist_sqr.py
+++ algorithms/benchmark/dist_sqr.py
@@ -1,5 +1,5 @@
 def dist_sqr(point_1: list[float], point_2: list[float]) -> float:
     squares_sum = 0
-    for axis in range(len(point_1)):
+    for axis, _ in enumerate(point_1):
         squares_sum += (point_1[axis] - point_2[axis])**2
     return squares_sum
