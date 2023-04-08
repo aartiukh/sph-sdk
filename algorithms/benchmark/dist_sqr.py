a52a83e80cce4eec1027c11089fd4d41fd8c64b0
Polina Artiukh <polina.zemko@rallyware.com>
2023-04-08 23:33:45 +0200

    Add docstring for dist_sqr

--- algorithms/benchmark/dist_sqr.py
+++ algorithms/benchmark/dist_sqr.py
@@ -1,5 +1,11 @@
 def dist_sqr(point_1: list[float], point_2: list[float]) -> float:
+    """
+    Calculates square of Euclidian distance for two points.
+    :param point_1: first point of kind list[float]
+    :param point_2: second point of kind list[float]
+    :return: float
+    """
     squares_sum = 0
     for axis, _ in enumerate(point_1):
-        squares_sum += (point_1[axis] - point_2[axis])**2
+        squares_sum += (point_1[axis] - point_2[axis]) ** 2
     return squares_sum
