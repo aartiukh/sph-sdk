393cb86e796cb4cbc5109faff920693052ab54be
Polina <51140452+paulzcooper@users.noreply.github.com>
2023-04-20 21:30:07 +0200

    Box search algorithm (#49)

---
+++ algorithms/test/benchmark/box_search.py
@@ -0,0 +1,118 @@
+import logging
+import sys
+
+from dist_sqr import dist_sqr
+
+LOG = logging.getLogger(__name__)
+logging.basicConfig(format='%(asctime)s[%(name)s:%(lineno)d] %(levelname)s %(message)s')
+
+
+class BoxSearch:
+    def __init__(self, search_radius: float, domain_size: float, epsilon: float = 10e-8, verbose: bool = False):
+        """
+        Initialize the box-based neighbors search algorithm.
+        :param search_radius: minimal distance for particles to become neighbors
+        :param epsilon: precision for distance calculation, set to 10e-8 by default
+        :param domain_size: upper bound for the working domain
+        """
+
+        self.search_radius = search_radius
+        self.epsilon = epsilon
+        self.domain_size = domain_size
+        self._verbose = verbose
+        self._boxes_in_row = round(self.domain_size / self.search_radius)
+        self._total_boxes = self._boxes_in_row ** 2
+        self._box_neighbors = self._find_neighbor_boxes()
+
+        if verbose:
+            LOG.setLevel(level=logging.DEBUG)
+            message = "Initialized BoxSearch: search_radius={sr}, domain_size={ds}, epsilon={eps}, " \
+                      "boxes_in_row={bxs}, total_boxes={tbxs}".format(sr=self.search_radius,
+                                                                      ds=self.domain_size,
+                                                                      eps=self.epsilon,
+                                                                      bxs=self._boxes_in_row,
+                                                                      tbxs=self._total_boxes)
+            LOG.debug(message)
+
+    def _find_neighbor_boxes(self) -> list[list]:
+        """
+        Finds neighbor boxes indexes for all boxes of the provided domain size.
+        :return: 2D list of neighbor boxes indexes for each box.
+        """
+        neighbor_boxes_ids = [[] for box in range(self._total_boxes)]
+        for row in range(self._boxes_in_row):
+            for col in range(self._boxes_in_row):
+                current_box_id = row * self._boxes_in_row + col
+
+                for x in range(max(0, row - 1), min(row + 1, self._boxes_in_row - 1) + 1):
+                    for y in range(max(0, col - 1), min(col + 1, self._boxes_in_row - 1) + 1):
+                        if x != row or y != col:
+                            neighbor_box_id = x * self._boxes_in_row + y
+                            neighbor_boxes_ids[current_box_id].append(neighbor_box_id)
+
+        if self._verbose:
+            for box_id, neighbor_boxes in enumerate(neighbor_boxes_ids):
+                message = "BOX {box} NEIGHBORS: {nb_bx}".format(box=box_id,
+                                                                nb_bx=neighbor_boxes)
+                LOG.debug(message)
+
+        return neighbor_boxes_ids
+
+    def _put_points_into_boxes(self, points: list[list]) -> list[list]:
+        """
+        Assigns a box index for each point based on its coordinates and returns list of associated points for each box
+        of the domain.
+        :param points: 2D list of kind [[p0x, p0y], [p1x, p1y], ...]
+        :return: 2D list of kind [[point_index, point_index, ...], [point_index, point_index, ...], ...],
+                where the list index is the index of the according associated box.
+        """
+        points_in_boxes = [[] for box in range(self._total_boxes)]
+
+        for point_index, point in enumerate(points):
+            col = int((point[0] + sys.float_info.epsilon) // self.search_radius)
+            row = int((point[1] + sys.float_info.epsilon) // self.search_radius)
+            box_id = int(row * self._boxes_in_row + col)
+            points_in_boxes[box_id].append(point_index)
+            if self._verbose:
+                message = "POINT_{p_idx}({point}): COL = {cl} ROW = {rw} BOX_ID = {bx_idx}".format(p_idx=point_index,
+                                                                                                   point=point,
+                                                                                                   cl=col,
+                                                                                                   rw=row,
+                                                                                                   bx_idx=box_id)
+                LOG.debug(message)
+
+        if self._verbose:
+            message = "TOTAL BOXES: {n_boxes}".format(n_boxes=len(points_in_boxes))
+            LOG.debug(message)
+            for box_id, points_ids in enumerate(points_in_boxes):
+                message = "BOX {box} POINTS: {pts_idx}".format(box=box_id,
+                                                               pts_idx=points_ids)
+                LOG.debug(message)
+
+        return points_in_boxes
+
+    def search(self, points: list[list]) -> list[list]:
+        """
+        Performs the box-based neighbors search on the passed points.
+        :param points: 2D list of kind [[p0x, p0y], [p1x, p1y], ...]
+        :return: 2D list of kind [[neighbor_point_index, neighbor_point_index, ...],  ...],
+                where the list index is the index of the according point.
+        """
+        neighbors = [[] for point in range(len(points))]
+        points_in_boxes = self._put_points_into_boxes(points)
+
+        for box_id, box_points_ids in enumerate(points_in_boxes):
+            curr_box_neighbors = self._box_neighbors[box_id]
+
+            for neighbor_box_id in curr_box_neighbors:
+                if neighbor_box_id >= box_id:
+                    neighbor_box_points = points_in_boxes[neighbor_box_id]
+
+                    for box_point_id in box_points_ids:
+                        for neighbor_point_id in neighbor_box_points:
+                            dist = dist_sqr(points[box_point_id], points[neighbor_point_id])
+                            if dist - self.search_radius ** 2 < self.epsilon:
+                                neighbors[box_point_id].append(neighbor_point_id)
+                                neighbors[neighbor_point_id].append(box_point_id)
+
+        return neighbors
---
+++ algorithms/test/benchmark/box_search_test.py
@@ -0,0 +1,341 @@
+import unittest
+
+from box_search import BoxSearch
+
+
+class TestBoxSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.search_radius = 0.2
+        self.epsilon = 10e-8
+        self.domain_size = 1.0
+        self._boxes_in_row = int(self.domain_size / self.search_radius)
+        self._total_boxes = self._boxes_in_row ** 2
+        self.init_cases = [
+            {  # 2x2 boxes
+                'input': {
+                    'search_radius': 0.2,
+                    'domain_size': 0.4,
+                    'epsilon': 10e-8,
+                },
+                'expected_output': {
+                    'boxes_in_row': 2,
+                    'total_boxes': 4
+                }
+            },
+            {  # 3x3 boxes
+                'input': {
+                    'search_radius': 0.4,
+                    'domain_size': 1.2,
+                    'epsilon': 10e-8,
+                },
+                'expected_output': {
+                    'boxes_in_row': 3,
+                    'total_boxes': 9
+                }
+            },
+            {  # 4x4 boxes
+                'input': {
+                    'search_radius': 0.3,
+                    'domain_size': 1.2,
+                    'epsilon': 10e-8,
+                },
+                'expected_output': {
+                    'boxes_in_row': 4,
+                    'total_boxes': 16
+                }
+            },
+            {  # 5x5 boxes
+                'input': {
+                    'search_radius': 0.2,
+                    'domain_size': 1.0,
+                    'epsilon': 10e-8,
+                },
+                'expected_output': {
+                    'boxes_in_row': 5,
+                    'total_boxes': 25
+                }
+            }
+        ]
+        self.neighbor_boxes_cases = [
+            {  # 2x2 boxes
+                'input': {
+                    'search_radius': 0.2,
+                    'domain_size': 0.4,
+                    'epsilon': 10e-8,
+                },
+                'expected_output': [
+                    # box neighbors
+                    [1, 2, 3],  # 0
+                    [0, 2, 3],  # 1
+                    [0, 1, 3],  # 2
+                    [0, 1, 2],  # 3
+                ]
+
+            },
+            {  # 3x3 boxes
+                'input': {
+                    'search_radius': 0.4,
+                    'domain_size': 1.2,
+                    'epsilon': 10e-8,
+                },
+                'expected_output': [
+                    # box neighbors
+                    [1, 3, 4],                  # 0
+                    [0, 2, 3, 4, 5],            # 1
+                    [1, 4, 5],                  # 2
+                    [0, 1, 4, 6, 7],            # 3
+                    [0, 1, 2, 3, 5, 6, 7, 8],   # 4
+                    [1, 2, 4, 7, 8],            # 5
+                    [3, 4, 7],                  # 6
+                    [3, 4, 5, 6, 8],            # 7
+                    [4, 5, 7],                  # 8
+                ]
+
+            },
+            {  # 4x4 boxes
+                'input': {
+                    'search_radius': 0.3,
+                    'domain_size': 1.2,
+                    'epsilon': 10e-8,
+                },
+                'expected_output': [
+                    # box neighbors
+                    [1, 4, 5],                      # 0
+                    [0, 2, 4, 5, 6],                # 1
+                    [1, 3, 5, 6, 7],                # 2
+                    [2, 6, 7],                      # 3
+                    [0, 1, 5, 8, 9],                # 4
+                    [0, 1, 2, 4, 6, 8, 9, 10],      # 5
+                    [1, 2, 3, 5, 7, 9, 10, 11],     # 6
+                    [2, 3, 6, 10, 11],              # 7
+                    [4, 5, 9, 12, 13],              # 8
+                    [4, 5, 6, 8, 10, 12, 13, 14],   # 9
+                    [5, 6, 7, 9, 11, 13, 14, 15],   # 10
+                    [6, 7, 10, 14, 15],             # 11
+                    [8, 9, 13],                     # 12
+                    [8, 9, 10, 12, 14],             # 13
+                    [9, 10, 11, 13, 15],            # 14
+                    [10, 11, 14],                   # 15
+                ]
+            },
+            {  # 5x5 boxes
+                'input': {
+                    'search_radius': 0.2,
+                    'domain_size': 1.0,
+                    'epsilon': 10e-8,
+                },
+                'expected_output': [
+                    # box neighbors
+                    [1, 5, 6],                          # 0
+                    [0, 2, 5, 6, 7],                    # 1
+                    [1, 3, 6, 7, 8],                    # 2
+                    [2, 4, 7, 8, 9],                    # 3
+                    [3, 8, 9],                          # 4
+                    [0, 1, 6, 10, 11],                  # 5
+                    [0, 1, 2, 5, 7, 10, 11, 12],        # 6
+                    [1, 2, 3, 6, 8, 11, 12, 13],        # 7
+                    [2, 3, 4, 7, 9, 12, 13, 14],        # 8
+                    [3, 4, 8, 13, 14],                  # 9
+                    [5, 6, 11, 15, 16],                 # 10
+                    [5, 6, 7, 10, 12, 15, 16, 17],      # 11
+                    [6, 7, 8, 11, 13, 16, 17, 18],      # 12
+                    [7, 8, 9, 12, 14, 17, 18, 19],      # 13
+                    [8, 9, 13, 18, 19],                 # 14
+                    [10, 11, 16, 20, 21],               # 15
+                    [10, 11, 12, 15, 17, 20, 21, 22],   # 16
+                    [11, 12, 13, 16, 18, 21, 22, 23],   # 17
+                    [12, 13, 14, 17, 19, 22, 23, 24],   # 18
+                    [13, 14, 18, 23, 24],               # 19
+                    [15, 16, 21],                       # 20
+                    [15, 16, 17, 20, 22],               # 21
+                    [16, 17, 18, 21, 23],               # 22
+                    [17, 18, 19, 22, 24],               # 23
+                    [18, 19, 23]                        # 24
+                ]
+            }
+        ]
+        self.points_cases = [
+            {  # 2x2 boxes
+                'input': {
+                    'search_radius': 0.2,
+                    'domain_size': 0.4,
+                    'epsilon': 10e-8,
+                    'points': [
+                        [0.1, 0.2],     # 0
+                        [0.3, 0.1],     # 1
+                        [0.2, 0.2]      # 2
+                    ]
+                },
+                'expected_output': [
+                    # box points
+                    [],     # 0
+                    [1],    # 1
+                    [0],    # 2
+                    [2],    # 3
+                ]
+
+            },
+            {  # 2x2 boxes
+                'input': {
+                    'search_radius': 0.25,
+                    'domain_size': 0.5,
+                    'epsilon': 10e-8,
+                    'points': [
+                        [0.1, 0.2],  # 0
+                        [0.3, 0.1],  # 1
+                        [0.3, 0.25]  # 2
+                    ]
+                },
+                'expected_output': [
+                    # box points
+                    [0],  # 0
+                    [1],  # 1
+                    [],  # 2
+                    [2],  # 3
+                ]
+
+            },
+            {  # 3x3 boxes
+                'input': {
+                    'search_radius': 0.4,
+                    'domain_size': 1.2,
+                    'epsilon': 10e-8,
+                    'points': [
+                        [0.4, 0.5],   # 0
+                        [0.9, 0.3],   # 1
+                        [0.5, 0.4],   # 2
+                        [0.2, 1.0],   # 3
+                    ]
+                },
+                'expected_output': [
+                    # box points
+                    [],      # 0
+                    [],      # 1
+                    [1],     # 2
+                    [],      # 3
+                    [0, 2],  # 4
+                    [],      # 5
+                    [3],     # 6
+                    [],      # 7
+                    [],      # 8
+                ]
+
+            },
+            {  # 4x4 boxes
+                'input': {
+                    'search_radius': 0.3,
+                    'domain_size': 1.2,
+                    'epsilon': 10e-8,
+                    'points': [
+                        [0.5, 0.5],  # 0
+                        [0.6, 0.3],  # 1
+                        [1.0, 0.7],  # 2
+                        [0.4, 0.6],  # 3
+                        [0.8, 0.8],  # 4
+                    ]
+                },
+                'expected_output': [
+                    # box points
+                    [],   # 0
+                    [],   # 1
+                    [],   # 2
+                    [],   # 3
+                    [],   # 4
+                    [0],  # 5
+                    [1],  # 6
+                    [],   # 7
+                    [],   # 8
+                    [3],  # 9
+                    [4],  # 10
+                    [2],   # 11
+                    [],   # 12
+                    [],   # 13
+                    [],   # 14
+                    [],   # 15
+                ]
+
+            },
+            {  # 5x5 boxes
+                'input': {
+                    'search_radius': 0.2,
+                    'domain_size': 1.0,
+                    'epsilon': 10e-8,
+                    'points': [
+                        [0.2, 0.2],   # 0
+                        [0.5, 0.4],   # 1
+                        [0.5, 0.6],   # 2
+                        [0.2, 0.7],   # 3
+                        [0.9, 0.75],  # 4
+                        [0.6, 0.35],  # 5
+                        [0.45, 0.5],  # 6
+                    ]
+                },
+                'expected_output': [
+                    # box points
+                    [],      # 0
+                    [],      # 1
+                    [],      # 2
+                    [],      # 3
+                    [],      # 4
+                    [],      # 5
+                    [0],     # 6
+                    [],      # 7
+                    [5],     # 8
+                    [],      # 9
+                    [],      # 10
+                    [],      # 11
+                    [1, 6],  # 12
+                    [],      # 13
+                    [],      # 14
+                    [],      # 15
+                    [3],     # 16
+                    [2],     # 17
+                    [],      # 18
+                    [4],     # 19
+                    [],      # 20
+                    [],      # 21
+                    [],      # 22
+                    [],      # 23
+                    []       # 24
+                ]
+
+            },
+        ]
+
+    def test_init(self):
+        for case in self.init_cases:
+            config = case['input']
+            expected_output = case['expected_output']
+            with self.subTest(config=case['input']):
+                actual_box_search = BoxSearch(search_radius=config['search_radius'],
+                                              domain_size=config['domain_size'])
+
+                self.assertEqual(actual_box_search._boxes_in_row, expected_output['boxes_in_row'])
+                self.assertEqual(actual_box_search._total_boxes, expected_output['total_boxes'])
+
+    def test_find_neighbor_boxes(self):
+        for case in self.neighbor_boxes_cases:
+            config = case['input']
+            expected_nb = case['expected_output']
+            with self.subTest(config=config):
+                actual_box_nb = BoxSearch(search_radius=config['search_radius'],
+                                          domain_size=config['domain_size'])._box_neighbors
+                actual_box_nb = [sorted(neighbors) for neighbors in actual_box_nb]
+
+                self.assertEqual(actual_box_nb, expected_nb)
+
+    def test_put_points_into_boxes(self):
+        for case in self.points_cases:
+            config = case['input']
+            expected_bp = case['expected_output']
+            with self.subTest(config=config):
+                actual_bp = BoxSearch(search_radius=config['search_radius'],
+                                      domain_size=config['domain_size'])._put_points_into_boxes(config['points'])
+                actual_bp = [sorted(box_points) for box_points in actual_bp]
+
+                self.assertEqual(actual_bp, expected_bp)
+
+
+if __name__ == '__main__':
+    unittest.main()
