028f327333b41e273fedab71379d84725a4abb95
Anton Artiukh <6399458+aartiukh@users.noreply.github.com>
2025-01-25 20:29:53 +0100

    Add SPH tests (#56)

---
+++ sph/test/src/SPHTestSuite.h
@@ -0,0 +1,25 @@
+/**
+ * @file SPHTestSuite.h
+ * @author Anton Artiukh
+ * @date Created Jan 25, 2025
+ **/
+
+#ifndef SPH_TEST_SUITE_H_26192C2023784EE0B4976A48A1A8772B
+#define SPH_TEST_SUITE_H_26192C2023784EE0B4976A48A1A8772B
+
+namespace SPHSDK
+{
+
+namespace TestEnvironment
+{
+
+class SPHTestSuite
+{
+public:
+    static void runSPH();
+};
+
+} // namespace TestEnvironment
+} // namespace SPHSDK
+
+#endif // SPH_TEST_SUITE_H_26192C2023784EE0B4976A48A1A8772B
