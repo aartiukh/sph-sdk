31c0510a263c4f264abb338877d6f4de707c0acd
Anton Artiukh <anton.artiukh@pantheon.io>
2025-01-25 19:34:23 +0100

    Remove 2d tests

--- algorithms/test/src/NeighboursSearchTestSuite.h
+++ algorithms/test/src/NeighboursSearchTestSuite.h
@@ -25,106 +25,13 @@ namespace TestEnvironment
 class NeighboursSearchTestSuite
 {
 public:
-
-    /// NeighboursSearch::search() tests
-
-    static void searchInOneBox();
-
-    static void searchInDifferentBoxesRightBottom();
-
-    static void searchInDifferentBoxesLeftBottom();
-
-    static void searchInDifferentBoxesRightTop();
-
-    static void searchInDifferentBoxesLeftTop();
-
-    static void searchInDifferentBoxesRightSide();
-
-    static void searchInDifferentBoxesLeftSide();
-
-    static void searchInDifferentBoxesCenterBottom();
-
-    static void searchInDifferentBoxesCenterTop();
-
-    static void searchInCornerOfBoxesCenter();
-
-    static void searchInCornerOfBoxesCorners();
-
-    //-------------------------------------------------
-
     static void searchInOneBox3D();
 
     static void searchInDifferentBoxesCenterBack3D();
 
     static void searchInDifferentBoxesCenterMiddle3D();
 
-    /// NeighboursSearch::insertPointsIntoBoxes() tests
-    static void insertPointsIntoBoxesCornerPoints();
-
-    static void insertPointsIntoBoxesBorderPoints();
-
-    static void insertPointsIntoBoxesInnerPoints();
-
-    static void insertPointsIntoBoxesCenterBoxFrame();
-
-    static void insertPointsIntoBoxesInnerFrame();
-
-    static void insertPointsIntoBoxesTopRightBox();
-
-    static void insertPointsIntoBoxesTopLeftBox();
-
-    static void insertPointsIntoBoxesBottomLeftBox();
-
-    static void insertPointsIntoBoxesBottomRightBox();
-
-    static void insertPointsIntoBoxesBottomCenterBox();
-
-    static void insertPointsIntoBoxesLeftCenterBox();
-
-    static void insertPointsIntoBoxesTopCenterBox();
-
-    static void insertPointsIntoBoxesRightCenterBox();
-
-    static void insertPointsIntoBoxesBottomLineBox();
-
-    static void insertPointsIntoBoxesLeftLineBox();
-
-    static void insertPointsIntoBoxesTopLineBox();
-
-    static void insertPointsIntoBoxesRightLineBox();
-
-    static void insertPointsIntoBoxes3x2();
-
-    static void insertPointsIntoBoxes4x2();
-
-    static void insertPointsIntoBoxes2x4();
-
-    static void insertPointsIntoBoxes8x3();
-
-    static void insertPointsIntoBoxes9x6();
-
-    /// NeighboursSearch::findNearByBoxes() tests
-    static void findNearbyBoxesTwoByTwo();
-
-    static void findNearbyBoxesFourByTwo();
-
-    static void findNearbyBoxesFourByFour();
-
-    static void findNearbyBoxesFourByThree();
-
-    static void findNearbyBoxesThreeByThree();
-
 private:
-
-    struct TestPoint
-    {
-        TestPoint(Point2F _position) : position(_position) {}
-
-        Point2F position;
-
-        SizetVector neighbours;
-    };
-
     struct TestPoint3F
     {
         TestPoint3F(Point3F _position) : position(_position) {}
@@ -134,22 +41,7 @@ private:
         SizetVector neighbours;
     };
 
-    using TestPoints = std::vector<TestPoint>;
     using TestPoints3D = std::vector<TestPoint3F>;
-
-    static void testSearch(/*in*/ const Rect& areaRect,
-                           /*in*/ FLOAT radius,
-                           /*in*/ FLOAT accuracy,
-                           /*in*/ TestPoints &points,
-                           /*in*/ const SizetVector& expectedBoxSizes,
-                           /*in*/ const VectorOfSizetVectors& expectedNeighbours);
-
-    static void testInsert(/*in*/ const Rect& areaRect,
-                           /*in*/ FLOAT radius,
-                           /*in*/ FLOAT accuracy,
-                           /*in*/ TestPoints& points,
-                           /*in*/ const SizetVector& expectedBoxSizes,
-                           /*in*/ const VectorOfSizetVectors& expectedPointsInBoxes);
 
     static void testSearch3D(const Cuboid&               cuboid,
                            FLOAT                      radius,
