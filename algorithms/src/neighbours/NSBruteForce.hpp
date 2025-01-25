ac8c227c3b3edee4a4505e0301b5066f454e533f
Anton Artiukh <anton.artiukh@pantheon.io>
2025-01-25 21:06:31 +0100

    Remove

--- algorithms/src/neighbours/NSBruteForce.hpp
+++ algorithms/src/neighbours/NSBruteForce.hpp
@@ -14,23 +14,8 @@ NSBruteForce<T>::NSBruteForce(const Volume& volume, FLOAT radius, FLOAT eps)
     , m_radius(radius)
     , m_eps(eps)
     , m_boxes(VectorOfSizetVectors())
-    {
-        const Cuboid cuboid = m_volume.getBoundingCuboid();
-
-        m_cuboid = cuboid;
-
-        m_normalizedCuboidWidth = static_cast<size_t>(m_cuboid.width / m_radius);
-        m_normalizedCuboidLength = static_cast<size_t>(m_cuboid.length / m_radius);
-        m_normalizedCuboidHeight = static_cast<size_t>(m_cuboid.height / m_radius); 
-
-        m_boxesNumber = static_cast<size_t>(m_normalizedCuboidWidth *
-                                            m_normalizedCuboidLength *
-                                            m_normalizedCuboidHeight);
-        m_boxes.resize(m_boxesNumber);
-        m_nearbyBoxes.resize(m_boxesNumber);
-
-        findNearbyBoxes();
-    }
+{
+}
 
 template <class T> NSBruteForce<T>::~NSBruteForce() = default;
 
