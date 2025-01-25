202310951573c13143ff1291c4581fffd726c241
Anton Artiukh <anton.artiukh@pantheon.io>
2025-01-25 22:59:10 +0100

    one more

--- algorithms/src/neighbours/NeighboursSearchInterface.h
+++ algorithms/src/neighbours/NeighboursSearchInterface.h
@@ -1,3 +1,9 @@
+/**
+ * @file NeighboursSearchInterface.h
+ * @author Anton Artiukh
+ * @date Created Jan 25, 2025
+ **/
+
 #ifndef NEIGHBOURS_SEARCH_INTERFACE_H_27DCCF3737274ADE9B6EEF8BF6947A6A
 #define NEIGHBOURS_SEARCH_INTERFACE_H_27DCCF3737274ADE9B6EEF8BF6947A6A
 
@@ -7,7 +13,6 @@ namespace SPHSDK
 template <class T> class NeighboursSearchI
 {
 public:
-
     virtual void search(T& points) = 0;
 };
 
