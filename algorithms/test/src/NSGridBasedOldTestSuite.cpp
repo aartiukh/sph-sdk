9a414263dd5331bf2c20839ac650bfaa3146b03f
Anton Artiukh <anton.artiukh@pantheon.io>
2025-01-26 13:42:37 +0100

    Remove other algorithms

--- algorithms/test/src/NSGridBasedOldTestSuite.cpp
+++ algorithms/test/src/NSGridBasedOldTestSuite.cpp
@@ -58,40 +58,6 @@ void NSGridBasedOldTestSuite::testSearch(
             EXPECT_EQ(expectedPointNeighbours[i], points[i].neighbours);
         }
     }
-
-    for (size_t i = 0u; i < points.size(); ++i)
-    {
-        points[i].neighbours.clear();
-    }
-
-    {
-        NSBruteForce<TestPoints3D> ns(volume, radius, accuracy);
-
-        ns.search(points);
-
-        for (size_t i = 0u; i < points.size(); ++i)
-        {
-            std::sort(points[i].neighbours.begin(), points[i].neighbours.end());
-            EXPECT_EQ(expectedPointNeighbours[i], points[i].neighbours);
-        }
-    }
-
-    for (size_t i = 0u; i < points.size(); ++i)
-    {
-        points[i].neighbours.clear();
-    }
-
-    {
-        NSBruteForceImproved<TestPoints3D> ns(volume, radius, accuracy);
-
-        ns.search(points);
-
-        for (size_t i = 0u; i < points.size(); ++i)
-        {
-            std::sort(points[i].neighbours.begin(), points[i].neighbours.end());
-            EXPECT_EQ(expectedPointNeighbours[i], points[i].neighbours);
-        }
-    }
 }
 
 void NSGridBasedOldTestSuite::testInsert(
